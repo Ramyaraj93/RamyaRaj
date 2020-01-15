c4993bf8ba844240dc7b30491f0e39f960dc63a3
Ramyaraj93 <ramyarajkailas3@gmail.com>
2020-01-15 22:46:56 +0530

    4thcommit

--- Flowcontrols/flowcontrols/secondlargest.py
+++ Flowcontrols/flowcontrols/secondlargest.py
@@ -1,6 +1,6 @@
-no1=int(input("Enter the first no"))
-no2=int(input("Enter the second no"))
-no3=int(input("Enter the third no"))
+no1=int(input("Enter the first no:"))
+no2=int(input("Enter the second no:"))
+no3=int(input("Enter the third no:"))
 
 if((no1>no2)&(no1<no3)):
     print("no1 is the second largest no")
