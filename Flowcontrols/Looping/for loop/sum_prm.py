b5d1b70a54fc43e1195210d739a10664fad2e256
Ramyaraj93 <ramyarajkailas3@gmail.com>
2020-01-17 09:27:58 +0530

    6thcommit

--- Flowcontrols/Looping/for loop/sum_prm.py
+++ Flowcontrols/Looping/for loop/sum_prm.py
@@ -1,12 +1,13 @@
 ll=int(input("Enter the lower limit:"))
 ul=int(input("Enter the upper limit:"))
 s=0
+
 for no in range(ll,ul+1):
    if(no>1):
        for i in range(2,no):
            if(no%i==0):
-               s+=no
-               print(s)
                break
        else:
            print(no)
+           s+=no
+print("sum=",s)
